1e176b5ed848ee064e27b509f5db227e3acd3bff
Basil Goldman <basil@inqwise.com>
2021-11-29 09:50:37 +0200

    added global css, html, hls

--- public/js/dai.js
+++ public/js/dai.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("\n\n// Test\nconsole.log('dai');\n\n//# sourceURL=webpack://dai/./src/index.js?");
+eval("console.log('dai');\n\n// This stream will be played if ad-enabled playback fails.\nvar BACKUP_STREAM = 'http://storage.googleapis.com/testtopbox-public/video_content/bbb/master.m3u8';\n\n// Live stream asset key\nvar TEST_ASSET_KEY = 'sN_IYUG8STe1ZzhIIE_ksA';\n\n// VOD content source and video IDs.\nvar TEST_CONTENT_SOURCE_ID = '2528370';\nvar TEST_VIDEO_ID = 'tears-of-steel';\n\n// StreamManager which will be used to request ad-enabled streams.\nvar streamManager;\n\n// hls.js\nvar hls = new Hls();\n\n// Video element\nvar videoElement;\n\n// Ad container\nvar adContainer;\n\n\n\n\n// TODO:\nvideoElement = document.getElementById('video-element');\nadContainer = document.getElementById('ad-container');\n\nloadUrl(BACKUP_STREAM);\n\nfunction loadUrl(url) {\n    console.log('Loading:' + url);\n    hls.loadSource(url);\n    hls.attachMedia(videoElement);\n    hls.on(Hls.Events.MANIFEST_PARSED, function() {\n        console.log('Video Play');\n        videoElement.muted = true;\n        //videoElement.load();\n        videoElement.play();\n    });\n}\n\n\n\n\n//# sourceURL=webpack://dai/./src/index.js?");
 
 /***/ })
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,46 @@
-
-
-// Test
 console.log('dai');
+
+// This stream will be played if ad-enabled playback fails.
+var BACKUP_STREAM = 'http://storage.googleapis.com/testtopbox-public/video_content/bbb/master.m3u8';
+
+// Live stream asset key
+var TEST_ASSET_KEY = 'sN_IYUG8STe1ZzhIIE_ksA';
+
+// VOD content source and video IDs.
+var TEST_CONTENT_SOURCE_ID = '2528370';
+var TEST_VIDEO_ID = 'tears-of-steel';
+
+// StreamManager which will be used to request ad-enabled streams.
+var streamManager;
+
+// hls.js
+var hls = new Hls();
+
+// Video element
+var videoElement;
+
+// Ad container
+var adContainer;
+
+
+
+
+// TODO:
+videoElement = document.getElementById('video-element');
+adContainer = document.getElementById('ad-container');
+
+loadUrl(BACKUP_STREAM);
+
+function loadUrl(url) {
+    console.log('Loading:' + url);
+    hls.loadSource(url);
+    hls.attachMedia(videoElement);
+    hls.on(Hls.Events.MANIFEST_PARSED, function() {
+        console.log('Video Play');
+        videoElement.muted = true;
+        //videoElement.load();
+        videoElement.play();
+    });
+}
+
+
